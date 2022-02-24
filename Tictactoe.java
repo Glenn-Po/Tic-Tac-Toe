cf7242e0bf8f32e02723374f5bb97e65afa9b678
Glenn-Po <glennfor72@gmail.com>
2022-02-24 15:00:17 +0100

    Initial commit

---
+++ Tictactoe.java
@@ -0,0 +1,194 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.*;
+//import java.util.Scanner;
+
+//TOD
+//check how to resolve position to board cell
+class Tictactoe{
+    String[] template;
+    char[][] board;
+    final char[] LEGAL_PLAYER = new char[]{'O', 'X'};
+    //colors
+    //GREEN, BRIGHT_YELLOW, YELLOW, RED, FUCHSIA, CYAN
+    //effects
+    //UNDERLINE, STRIKETHROUGH, BOLD
+    private void init(){
+        this.template = new String[]{
+                " ------------------",
+                " |  *  |  *  | *  |",
+                " |-----+-----+----|",
+                " |  *  |  *  | *  |",
+                " |-----+-----+----|",
+                " |  *  |  *  | *  |",
+                " ------------------"
+                };
+
+        this.board = new char[][]{
+                new char[]{'1', '2', '3'},
+                new char[]{'4', '5', '6'},
+                new char[]{'7', '8', '9'}
+        };
+    }
+
+    private boolean isAvailable(int pos){
+        if(pos<1 || pos>9) return false;
+        //int row = ((pos % 3) + 2)%3, column =  ((pos+2) / 3) - 1; //correct later
+        pos = pos - 1;//resolve from 1 to 9 to 0-8
+        int row = pos/3, column = pos%3;
+        return !(this.board[row][column] == this.LEGAL_PLAYER[0] || this.board[row][column] == this.LEGAL_PLAYER[1]);
+    }
+    private void playAt(int pos, char player){
+        //this assumes that the position is available
+        //int x =((pos % 3) + 2)%3, y = ((pos+2) / 3) - 1;
+        pos = pos - 1;//resolve from 1 to 9 to 0-8
+        int row = pos/3, column = pos%3;
+        this.board[row][column] = player;
+
+    }
+    private void fillTemplate(){
+        for(byte in = 0; in < this.template.length; ++in)
+            this.template[in] = this.template[in].replace("*", "%c");
+
+        /*for(byte line = 0; line<3 ; line++){
+            for(byte column = 0; column < 3; column++){
+                this.template[(line+1)*2][5+column*6] = this.board[line][column];
+            }
+        }*/
+}
+    private void printBoard() {
+        this.fillTemplate();
+        int in = 0;
+        for (String line : this.template) {
+            if((in+1)%2 == 0)
+                System.out.printf(line, this.board[in/2][0],this.board[in/2][1],this.board[in/2][2]);
+            else
+                System.out.print(line);
+            System.out.println();
+            in++;
+        }
+    }
+
+    public char checkForWin(){
+        //check leading diagonal
+        if(this.board[0][0]==board[1][1] && board[1][1]==board[2][2])
+            return board[0][0];
+        //check alternate diagonal
+        if(this.board[0][2]==board[1][1] && board[1][1]==board[2][0])
+            return board[0][2];
+        //check the columns
+        for(byte col=0; col<3; ++col){
+            if(this.board[0][col]==board[1][col] && board[1][col]==board[2][col])
+                return board[1][col];
+        }
+        //check the rows
+        for(byte row=0 ; row<3; ++row){
+            if(this.board[row][0]==this.board[row][1] && this.board[row][1]==this.board[row][2])
+                return this.board[row][0];
+        }
+        //if no win detected, return space char
+        return ' ';
+    }
+
+    //helper method to check if a particular player has already played on a winning line
+    private boolean hookedHere(char one, char two, char three){
+        boolean first = (one== LEGAL_PLAYER[0] || two ==LEGAL_PLAYER[0] || three ==LEGAL_PLAYER[0]),
+                second = (one== LEGAL_PLAYER[1] || two ==LEGAL_PLAYER[1] || three ==LEGAL_PLAYER[1]);
+
+        return (first&second);
+    }
+    public boolean isATie(){
+
+        //check for tie by checking all winning opportunities and identifying that none is possible
+        boolean winningSpotFound = false;
+
+        //first assume the game is a tie then try to find possibility of a win
+
+        //it is a tie if 2 opposing players are in the same winning line for all 6 winning lines
+        //check leading diagonal
+        if(!this.hookedHere(board[0][0], board[1][1], board[2][2]))
+            winningSpotFound = true;
+        //check alternate diagonal
+        if(!this.hookedHere(this.board[0][2], board[1][1], board[2][0]))
+            winningSpotFound = true;
+        //check the columns
+        for(byte col=0; col<3; ++col){
+            if (!this.hookedHere(this.board[0][col], board[1][col], board[2][col])) {
+                winningSpotFound = true;
+                break;
+            }
+        }
+        //check the rows
+        for(byte row=0 ; row<3; ++row){
+            if (!this.hookedHere(this.board[row][0], board[row][1], board[row][2])) {
+                winningSpotFound = true;
+                break;
+            }
+        }
+
+        return !winningSpotFound;
+    }
+
+    private int getPlayerPos() {
+        // Scanner scan = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean posGotten = false;
+        int pos = 0;
+        while(!posGotten){
+            System.out.print("Enter a position to play in: ");
+            try {
+                pos = Integer.parseInt(String.valueOf(reader.readLine().strip().toCharArray()[0]));
+            } catch (NumberFormatException e) {
+                System.out.println("Number Format not Correct::");
+            } catch (IOException e) {
+                System.out.println("Input Error: Error accepting input");
+                //e.printStackTrace();
+            }
+            if(this.isAvailable(pos))
+                posGotten = true;
+            else System.out.print("Unavailable--o_o--\n");
+        }
+        return pos;
+    }
+    
+    public void playGame(){
+
+        System.out.println("============================");
+        this.printBoard();
+        char Player1 = this.LEGAL_PLAYER[0], Player2=this.LEGAL_PLAYER[1],
+        curPlayer = Player1;
+        while(true){
+            if(this.isATie()){
+                System.out.println("Game is a TIE!!!!!!!!!");
+                break;
+            }
+            char winner  = checkForWin();
+            if(winner != ' '){
+                System.out.println("Game Over .. Player \""+winner+ "\" wins (^_^) + (-_-) + (*_*)");
+                break;
+            }
+
+            System.out.println("PLAYER "+(curPlayer)+" Play: ");
+            this.playAt(this.getPlayerPos(), curPlayer);
+
+            if(curPlayer==Player1 )
+                curPlayer = Player2;
+            else if(curPlayer == Player2)
+                curPlayer = Player1;
+            this.printBoard();
+        }
+        
+
+    }
+    public static void main(String[] args){
+        System.out.print("\033[92m     Tic Tac Toe");
+        System.out.print("\033[95m   Tic Tac Toe");
+        System.out.print("\033[97m     Tic Tac Toe");
+        System.out.print("\033[93m   Tic Tac Toe\n");
+        Tictactoe newGame = new Tictactoe();
+        newGame.init();
+        newGame.printBoard();
+        newGame.playGame();
+    }
+}
---
+++ tic_tac_toe.cpp
@@ -0,0 +1,749 @@
+//[]Tic-tac-toe
+
+//TODO
+//--reimplement tie checker
+//--implement spectacular endgame display
+
+#include "..\\..\\MACROS.h"
+#include <iostream>
+#include <conio.h>
+#include <cstdio>
+#include <cctype>
+#include <iomanip>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+
+#include <limits.h>
+
+#define INFINITY (2<<8)
+#define MIN_SCORE -(2<<3)
+#define MAX_SCORE -MIN_SCORE
+
+using std::cout;
+using std::cin;
+using std::vector;
+using std::string;
+using std::max;
+using std::min;
+
+void instructionManual();
+
+int checkForWinner(vector< vector<int> >& states);
+
+bool checkForTie(vector< vector<int> >& states);
+
+bool canPlayAtThisPos(vector< vector<int> >& states, int pos){
+
+	int x =((pos % 3) + 2)%3, y = ((pos+2) / 3) - 1;
+	return states[x][y] == 0;
+}
+
+void showWelcomeScreen();
+
+void displayAuthorInfo();
+
+void writePlayerIcon(int , char);
+
+void getGameInfo();//chooseChar();
+//choosw intelligence or random, char
+
+void drawBoard();
+
+void updateBoard(vector< vector<int> >&  v, int position, int player);
+
+void resetBoard(vector< vector<int> >& );
+
+int runGame(vector< vector<int> >&);//main game funtion
+
+void displayStatus(string msg, int type = 1 , int waitTime = 20);
+
+void writePositions();
+
+int getPosition(vector< vector<int> >&);
+
+int getComputerPlayPos(vector<vector<int> >& board);
+
+bool displayGameEndOrContinue(int winner);
+
+int getBestMove(const vector< vector<int> >& states);
+
+
+/****************************************************************************
+ *                                                                          *
+ * Function: main                                                           *
+ *                                                                          *
+ * Purpose : run and manage the game								        *
+ *                                                                          *
+ * History : Date      Reason                                               *
+ *           --/--/--  Created                                              *
+ *                                                                          *
+ ****************************************************************************/
+
+main(){
+
+	//0 still empty
+	//1 player
+	//-1 computer
+	/*
+	int states[][3]={
+		{0,  0,  0},
+		{0,  0,  0},
+		{0,  0,  0}
+	};*/
+	
+	showWelcomeScreen();
+	clear_screen();
+	
+	vector< vector<int> >  board;
+	board.clear();
+	board.push_back(vector<int>(3,0));
+	board.push_back(vector<int>(3,0));
+	board.push_back(vector<int>(3,0));
+	
+	while(true)
+	{
+		resetBoard(board);
+		instructionManual();
+		clear_screen();
+		drawBoard();
+		writePositions();
+		int winner = runGame(board);
+		Sleep(2000);
+    	clear_screen();
+		if(displayGameEndOrContinue(winner))
+			continue;
+		else
+			break;
+	}
+}
+
+
+
+//function to display status eg, input error
+int runGame(vector<vector<int> >& board){
+
+	bool gameOn = true;
+	int winner = 0 , chosenPosition;
+	
+	//0 for tie, 1 for player -1 for computer
+	
+	const char HUMAN = 'O' , COMPUTER = 'X'; //case of human and computer
+	
+	const char PLAYER_ONE = 'O', PLAYER_TWO = 'X'; //case of human and computer
+	
+	char nextPlayer = HUMAN;//for user
+	
+	displayStatus(string("Game is loading Up ")+string(10, '\xF9'), 1, 50);
+	Sleep(500);
+	displayStatus(string("The Game is On, You Are UP"), 1, 50);
+	Sleep(500);
+	
+	while(gameOn)
+	{
+		winner = checkForWinner(board);
+		if(winner or checkForTie(board))
+		{
+			gameOn = false;
+			continue;
+			//interestingly this two statements combine to from a break;
+		}
+		if(nextPlayer == HUMAN)
+		{
+			chosenPosition = getPosition(board);
+			updateBoard(board, chosenPosition, 1);
+			writePlayerIcon(chosenPosition, nextPlayer);
+            nextPlayer = COMPUTER;
+		}
+		else if(nextPlayer == COMPUTER)
+		{
+			chosenPosition = getBestMove(board);
+			//getComputerPlayPos(board);
+			updateBoard(board, chosenPosition, -1);
+			Sleep(1500);//sleeo to slow down the game (computer reaction)
+			writePlayerIcon(chosenPosition, nextPlayer);
+			Sleep(800);//sleeo to slow down the game and make it fun
+			displayStatus(string("Computer Plays, You are Up Next:)"));
+            nextPlayer = HUMAN;
+		}
+		
+	}
+	return winner;
+}
+
+
+
+//reset game to unplayed state
+void updateBoard(vector< vector<int> >&  v, int position, int player){
+	int x =((position % 3) + 2)%3, y = ((position+2) / 3) - 1;
+	v[x][y]= player;
+
+}
+
+
+void resetBoard(vector< vector<int> >&  v){
+	for(int i = 0; i < 3; ++i)
+	    for(int j = 0; j < 3; ++j)
+	        v[i][j] = 0;
+}
+
+
+void instructionManual(){
+
+	clear_screen();
+	TEXTCOLOR(AQUA);
+	cout<<"\n\n\n"
+	"    ***********************************************************************\n"
+	"    *                                                                     *\n"
+	"    *                       HELP AND TIPS TO PLAY                         *\n"
+	"    *                                                                     *\n"
+	"    ***********************************************************************\n"
+	"    *                                                                     *\n"
+	"    *    -The grid appears initially filled with numbers                  *\n"
+	"    *                                                                     *\n"
+	"    *    -This numbers represent positions that you can choose to play in *\n"
+	"    *                                                                     *\n"
+	"    *    -You can choose a number representing a grid position by         *\n"
+	"    *      entering the number at a prompt that will be indicated         *\n"              
+	"    *                                                                     *\n"
+	"    *                                                                     *\n"
+	"    *    -IT IS ASSUMED THAT YOU KNOW HOW A GAME OF TIC-TAC-TOE WORKS     *\n"
+	"    *                                                                     *\n"
+	"    *                                                                     *\n"
+	"    ***********************************************************************\n";
+	
+	cout<<"\n\n\nPress Any Key to continue\n\n";
+	getch();
+}
+
+
+void showWelcomeScreen(){
+
+    clear_screen();
+	GOTOXY(20, 4);
+	TEXTCOLOR(SHINYWHITE);
+	
+	char msg[] =
+	"WELCOME TO A GAME OF TIC TAC TOE \n\n\n"
+	"\tDEVELOPED BY: @Glenn-Po,\n\n\n"
+	"\tThis is a small, simple CONSOLE-BASED game.\n\n"
+ 	"\t\tENSURE to run under DOS on a PC\n\n\n"
+	"\tDisfruta el juego, Amigo";
+	
+	for(int i = 0; msg[i] != '\0' ;){
+        TEXTCOLOR(SHINYWHITE);
+        if(i >= 0 and i <= 32)
+            BTEXTCOLOR(SHINYRED, BLUE);
+		if(i >= 50 and i <= 50+34)
+		    TEXTCOLOR(GREEN);
+		cout << msg[i++];
+		Sleep(20);
+	}
+	Sleep(2000);
+	
+	TEXTCOLOR(DEFAULT);
+	cout << "\n\n\n\n\t\tHit Any Key To Continue . . .";
+	getch();
+}
+
+
+void drawBoard(){
+
+	TEXTCOLOR(AQUA);
+	const int GRID_HEIGHT(18);//16
+	const int GRID_WIDTH (19+5);//20
+	int counter, i, j;
+	
+	GOTOXY(0,4);
+	
+	std::cout << "\t";//8 spaces
+	
+	for(i = 0;  i < GRID_WIDTH + 1;  ++i)
+		std::cout << "*";// just print line
+		//cout<<'-';// just print line  -second choice
+
+	std::cout << std::endl;
+	
+	for( i = 1; i <= GRID_HEIGHT; ++i){
+        std::cout << "\t";//16 spaces
+		for(counter = 0, j = 0; counter < GRID_WIDTH+1, j < GRID_WIDTH + 1; ++counter, ++j)
+		{
+	        if(i%6)
+			{// when i%4!=0 signifying it is not a horizontal line
+			      if(counter == ((j/8)*8))// making use of integer division truncation in(j/5)
+			            std::cout << "*";//cout<<'|';// just print line  -second choice
+
+			      else std::cout << ' ';
+
+	         }
+	    	else std::cout << "*";//cout<<'-';// just print line  -second choice
+
+		}
+		std::cout << std::endl;
+	}
+}
+
+
+void writePositions(){
+
+	TEXTCOLOR(SILVER);
+	
+	int fxpos=12, fypos =  7, count = 1;
+	
+	for(int k = 0; k < 3; ++k)
+		for(int i=0; i < 3; ++i)
+			GOTOXY(fxpos+i*8, (fypos+k*6)), cout << count++;
+
+}
+
+
+void writePlayerIcon(int position, char playerIcon){
+
+	int COLOR = playerIcon == 'X'? SHINYRED : GREEN;
+	TEXTCOLOR(COLOR);
+	
+	int fxpos = 12, fypos =  7, count = 1,
+	xOffset =((position % 3) + 2)% 3, yOffset = ((position+2) / 3) - 1;
+	
+	GOTOXY(fxpos + xOffset * 8, (fypos+yOffset*6)), cout << playerIcon;
+}
+
+
+int checkForWinner(vector< vector<int> >& states){
+
+    for (int i = 0; i < 3; ++i)//rows
+        if (states[i][0] == states[i][1] and states[i][1] == states[i][2] and
+            states[i][1] != 0)
+                return states[i][1];//but should probebly return the winningxter
+        
+    for (int i = 0; i <3; ++i)//columns
+         if (states[0][i] == states[1][i] and states[1][i] == states[2][i] and
+            states[1][i] != 0)
+                return states[1][i];
+
+    if (states[0][0] == states[1][1] and states[1][1] == states[2][2]
+		and states[1][1] != 0)//leading diagonal
+		        return states[1][1];
+    
+    if (states[2][0] == states[1][1] and states[1][1] == states[0][2]
+		and states[1][1] != 0)//alternate diagonal
+		        return states[2][0];
+	return 0;
+}
+
+
+//!!Check the logic of this function again
+bool isNotATie(int pos1, int pos2, int pos3){
+
+	//cthis function checks for a tie in a line that you could win in
+	bool conditionForATie = not ((pos1 == pos2 and pos1 != 0) or
+					 (pos1 == pos3 and pos1 != 0) or
+					 (pos3 == pos2 and pos3 != 0))  ;
+					 
+	return conditionForATie ;
+}
+
+//checks for a not tie area
+
+bool checkForTie(vector< vector<int> >& states){
+
+	//check all the rows
+    for (int i = 0; i < 3; ++i)
+        if (isNotATie(states[i][0], states[i][1], states[i][2]))
+                return false;
+
+	//check all the columns
+    for (int i = 0; i <3; ++i)
+        if (isNotATie(states[0][i], states[1][i], states[2][i]))
+                return false;
+                
+    // check in the leading diagonal
+    if (isNotATie(states[0][0], states[1][1], states[2][2]))
+                return false;
+
+    //check the alternate diagonal
+    
+    if (isNotATie(states[0][2], states[1][1], states[2][0]))
+                return false;
+
+	//if code reaches here obviously no tie
+
+    return true;
+}
+
+//this has to make sure that the position has not been chosen alreay
+int getPosition(vector< vector<int> >& states){
+
+	char BACKSPACE = '\b', RETURN = '\r'; 
+	
+	bool posGotten = false, readyToSubmit = false,
+	    consoleIsCurrentlyDisplayingNum = false;
+	    
+	char pos = 0;
+	
+	TEXTCOLOR(GREEN);
+	GOTOXY(40, 7), cout << "Enter a position to play: [   ]";
+	GOTOXY(40+28, 7);
+	TEXTCOLOR(LIGHTAQUA);
+	
+	while(not posGotten or not readyToSubmit)
+	{
+		if(kbhit())
+		{
+			char ch = getch();
+			
+			if(std::isdigit(ch) and not posGotten and ch != '0'
+					and not consoleIsCurrentlyDisplayingNum)
+			{
+                cout << ch;
+				pos = ch;
+				consoleIsCurrentlyDisplayingNum = true;
+				posGotten = true;
+			}
+			else if(ch == BACKSPACE and consoleIsCurrentlyDisplayingNum)
+			{
+				consoleIsCurrentlyDisplayingNum = false;
+                posGotten = false;
+                cout << BACKSPACE << " " << BACKSPACE;
+			}
+			else if(ch == RETURN and posGotten)
+			{
+			    readyToSubmit = true;
+			    if(canPlayAtThisPos(states, pos-'0'))
+				;
+				else
+				{
+					displayStatus(string("This Position is unavailable"), -1, 40);
+				    readyToSubmit = posGotten = false;
+				    GOTOXY(40+29, 7);
+					TEXTCOLOR(LIGHTAQUA);
+				}
+			}
+		} //end if(kbhit())
+	}
+	
+	GOTOXY(40, 7), cout << string(5, '\t');
+	return pos-'0';
+}
+
+void displayStatus(string msg, int type , int waitTime){
+
+	//1 for normal - green
+	//0 for warning warning   -green
+	//-1 for
+	
+	int color = type == 1?SHINYGREEN : type == -1?SHINYRED : SHINYYELLOW;
+	TEXTCOLOR(color);
+	GOTOXY(8,2);
+	for(int i = 0; i < msg.length(); ++i)
+	{
+		cout << msg[i];
+		Sleep(waitTime);
+	}
+	
+	Sleep(100);
+	GOTOXY(8,2), cout << string(msg.length()+2, ' ');
+	TEXTCOLOR(DEFAULT);
+}
+
+int spotter(int ,int);
+
+int getComputerPlayPos(vector<vector<int> >& board){
+
+	//no need to check for valid position as the tie checher will do so
+	vector<int> possibleSpots;
+	
+	for(int i = 0; i < 3; ++i)
+	    for(int j = 0; j < 3; ++j)
+	        if (board[i][j] == 0)
+			{
+				int spot = spotter(i, j);
+	            possibleSpots.push_back(spot);
+			}
+	//return a random move choice
+	srand(time(NULL));
+	return possibleSpots[rand()%possibleSpots.size()];
+}
+
+//covert (i, j) to single index
+int spotter(int i  , int j){
+
+	for(int mx = 1; mx<=9; ++mx)
+		if((((mx%3)+2)%3) == i and j == ((mx+2)/3-1))//
+			return mx;
+	//cannot reach here??
+	return 1;
+}
+
+
+//make spectaular display
+//boxing animation of text with color:
+//red for lose, green for win, yellow for draw
+// and with respective emojis
+
+//message smaal: "you", big:"msg; lose, win, tie"  + for win, - for lose | for draw
+bool displayGameEndOrContinue(int winner){
+
+	string message;
+	switch(winner)
+	{
+		case 0:
+			message = 
+			"00000  000  00 0000  0   0 00000 0000 0000   \n"
+			"0     0   0 00 0   0 0   0   0   0    0   0  \n"
+			"0 000 00000 00 0 00  0   0   0   0000 0000   \n"
+			"0   0 0   0 00 0     0   0   0   0    0   0  \n"
+			"00000 0   0 00   0 0     00000   0   0000 0    0 \n";
+			break;
+		case 1:
+			message = 
+			"0   0 0   0 0   0  000  0   0 \n"
+			"0   0 0   0 00 00 0   0 00  0 \n"
+			"0 0 0 0   0 0 0 0 00000 0 0 0 \n"
+			"0   0 0   0 0   0 0   0 0  00 \n"
+			"0   0 00000 0   0 0   0 0   0 \n";
+			break;
+		case -1:
+			message = 
+			"00000 00000 0   0 0000  0   0 00000 0000 0000   \n"
+			"0     0   0 00 00 0   0 0   0   0   0    0   0  \n"
+			"0     0   0 0 0 0 0 00  0   0   0   0000 0000   \n"
+			"0     0   0 0   0 0     0   0   0   0    0   0  \n"
+			"00000 00000 0   0 0     00000   0   0000 0    0 \n";
+			break;
+			break;
+
+	}
+	
+	//annimate the message
+	int color = 1, nCnt = 0;
+	const int NUMBER_OF_TIMES_TO_MARQUEE = message.length();
+	while(nCnt < NUMBER_OF_TIMES_TO_MARQUEE)
+	{
+		TEXTCOLOR(color);
+		GOTOXY(14, 10), std::cout << message;// write out first
+	    char hold = message[0];
+	    message.erase(0,1);
+	    message += hold;
+	    Sleep(75);
+		color = color == 15 ? 1: color + 1;//first 15 colors are 'background-less'
+		nCnt++;
+	}
+	clear_screen();
+	Sleep(500);
+	
+	
+	//prompt to play again
+	
+	char doYouWantToPlayAgain = 'n';
+	TEXTCOLOR(SHINYGREEN);
+	GOTOXY(14, 10), std::cout << "Do You Want To Play Again [y/n]: ";
+	TEXTCOLOR(LIGHTAQUA);
+
+	//implement this here
+	
+/*	while( (doYouWantToPlayAgain = getch()) and
+			(tolower(doYouWantToPlayAgain) !='n' and
+			tolower(doYouWantToPlayAgain) != 'y'));
+			
+			cout << doYouWantToPlayAgain
+			;*/
+    doYouWantToPlayAgain = getche();
+    
+	switch(doYouWantToPlayAgain)
+	{
+		case 'y':
+		case 'Y':
+			Sleep(500);
+			return true;
+		case 'n':
+		case 'N':
+			clear_screen();
+			TEXTCOLOR(SHINYGREEN);
+			GOTOXY(14, 10), cout << "GOOD BYE | Hasta la vista \2\2\n\n See You Next Time!!!!";
+			Sleep(1000);
+			TEXTCOLOR(DEFAULT);
+			cout << "\n\n\n\n\t\t"
+			    << " Hit Any key to exit the console";
+			getch();
+			return false;
+		default:
+			clear_screen();
+			TEXTCOLOR(SHINYRED);
+			GOTOXY(14, 10), cout << "Unknown Choice!!!!!!!!!!";
+			Sleep(1000);
+			TEXTCOLOR(DEFAULT);
+			cout << "\n\tEXITING......\n\n\n\t\t"
+			    << " Hit Any key to exit the console";
+			getch();
+			return false;
+	}
+}
+
+
+/*##############################################################################
+Win: If you have two in a row, play the third to get three in a row.
+
+Block: If the opponent has two in a row, play the third to block them.
+
+Fork: Create an opportunity where you can win in two ways.
+
+Block Opponent's Fork:
+
+Option 1: Create two in a row to force the opponent into defending, as long as it doesn't result in them creating a fork or winning. For example, if "X" has a corner, "O" has the center, and "X" has the opposite corner as well, "O" must not play a corner in order to win. (Playing a corner in this scenario creates a fork for "X" to win.)
+
+Option 2: If there is a configuration where the opponent can fork, block that fork.
+
+Center: Play the center.
+
+Opposite Corner: If the opponent is in the corner, play the opposite corner.
+
+Empty Corner: Play an empty corner.
+
+Empty Side: Play an empty side.
+#################################################################################################
+*/
+
+
+int sudoAIPositionFinder(vector< vector<int> >& board){
+	
+	vector<int> possibleSpots;
+	
+	for(int i = 0; i < 3; ++i)
+	    for(int j = 0; j < 3; ++j)
+	        if (board[i][j] == 0)
+			{
+				int spot = spotter(i, j);
+	            possibleSpots.push_back(spot);
+			}
+	//return a calculate move choice
+	
+	//choice operator : evade loose Vs Go for win
+	
+	int xOffset = 0, yOffset = 0;
+	//search for valid spot
+	//example board
+	//   1 -1  0
+	//   0  0  1
+	//  -1  0  1
+	
+	//flatten the states vector
+		int definitvelyCalculatedSpot = 0;//dummy
+	
+	return possibleSpots[definitvelyCalculatedSpot];
+}
+
+///absolute ai position finder using minimax
+///unbeatable
+
+int minimaxAISpotFinder(vector< vector<int> > board, int depth, int player);
+
+int getBestMove(const vector< vector<int> >& board){
+
+	vector<int>  emptySpots;
+
+	for(int i = 0; i < 3; ++i)
+	    for(int j = 0; j < 3; ++j)
+	        if (board[i][j] == 0)
+			{
+	            emptySpots.push_back(spotter(i, j));
+			}
+			
+			
+	int bestMove;
+	
+	int bestScore = -INFINITY;
+	
+	for(int i = 0; i < emptySpots.size(); i++)
+	{
+		
+		int spot = emptySpots.at(i);
+		
+		vector <vector<int> > boardCopy(board);
+		
+		//play at this spot and pass the states to minimax
+		updateBoard(boardCopy, spot, -1);
+		
+		int score = minimaxAISpotFinder(boardCopy, 0, 1);
+		
+		//infact if the score is 1 i should just proceed and play... right?
+		if(score > bestScore)
+		{
+			bestScore = score;
+			bestMove = spot;
+		}
+	}
+		
+	return bestMove;
+}
+
+
+//return the score
+//depth: 0 at first
+//player: -1 for computer, 1 for human
+int minimaxAISpotFinder(vector< vector<int> > board, int depth, int player)
+{
+	//check for tie or win [terminal state]
+	int winner = checkForWinner(board);
+	//the score correlates negatively with the value (base on 
+	//the fact that the computer is maximising
+	if(winner){
+		if(winner == -1)//computer
+			return MAX_SCORE - depth;
+		else
+			return MIN_SCORE + depth;
+	}
+	if(checkForTie(board))return 0;
+	
+	//not terminal
+	
+	bool isMaximising = (player == -1); //ie computer
+	
+	vector<int>  emptySpots;
+	for(int i = 0; i < 3; ++i)
+	    for(int j = 0; j < 3; ++j)
+	        if (board[i][j] == 0)
+			{
+	            emptySpots.push_back(spotter(i, j));
+			}
+	
+	
+	if(isMaximising)
+	{
+		int bestScore = -INFINITY;
+		for(int i = 0; i < emptySpots.size(); i++)
+		{
+			int spot = emptySpots.at(i);
+			///
+			vector <vector<int> > boardCopy(board);
+		
+			updateBoard(boardCopy, spot, player);
+			///
+			int score = minimaxAISpotFinder(boardCopy, depth+1, -player);
+			bestScore = max(bestScore, score);
+		}
+		
+		return bestScore;
+	}
+	else
+	{
+		int bestScore = INFINITY;
+		for(int i = 0; i < emptySpots.size(); i++)
+		{
+			int spot = emptySpots.at(i);
+			///
+			vector <vector<int> > boardCopy(board);
+		
+			updateBoard(boardCopy, spot, player);
+			///
+			int score = minimaxAISpotFinder(boardCopy, depth+1, -player);
+			bestScore = min(bestScore, score);
+		}
+		
+		return bestScore;
+	
+	}
+	
+}
+
